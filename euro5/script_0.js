95d9948358cb35ea15e9158173300afbbf05556b
arthurs-code <arthur.stivenson@gmail.com>
2026-07-19 15:33:46 +0200

    Add files via upload
    test V_209

--- euro5/script_0.js
+++ euro5/script_0.js
@@ -1,1 +1,1 @@
-(function(){try{var m=localStorage.getItem('easyGym.enDeFrItEs.theme.v1');if(m==='light'||m==='dark'){document.documentElement.setAttribute('data-theme',m);}}catch(e){}})();
+document.documentElement.setAttribute('data-theme','dark');
