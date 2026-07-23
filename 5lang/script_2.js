7c942d4c5c9c2853b0951dd4477ae121f05d4caf
arthurs-code <arthur.stivenson@gmail.com>
2026-07-23 05:28:22 +0200

    Add files via upload
    PWA v292X

--- 5lang/script_2.js
+++ 5lang/script_2.js
@@ -1,1 +1,5 @@
-if('serviceWorker' in navigator && location.protocol.startsWith('http')) navigator.serviceWorker.register('service-worker.js').catch(()=>{});
+if('serviceWorker' in navigator && location.protocol.startsWith('http')){
+  navigator.serviceWorker.register('./service-worker.js?v=292X',{updateViaCache:'none'})
+    .then(registration=>registration.update())
+    .catch(()=>{});
+}
