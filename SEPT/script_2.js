e61a91aca14dbad8da72d4f7c20ceb0a36a53370
arthurs-code <arthur.stivenson@gmail.com>
2026-09-17 06:57:46 +0200

    Delete SEPT directory

--- SEPT/script_2.js
+++
@@ -1,5 +0,0 @@
-if('serviceWorker' in navigator && location.protocol.startsWith('http')){
-  navigator.serviceWorker.register('./service-worker.js?v=313X',{updateViaCache:'none'})
-    .then(registration=>registration.update())
-    .catch(()=>{});
-}
