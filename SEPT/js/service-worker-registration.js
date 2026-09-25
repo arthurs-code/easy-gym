dd5d1891da853c919201bf0c198b53687d4535ca
arthurs-code <arthur.stivenson@gmail.com>
2026-09-25 02:42:09 +0200

    Add files via upload

--- SEPT/js/service-worker-registration.js
+++ SEPT/js/service-worker-registration.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator && location.protocol.startsWith('http')){
-  navigator.serviceWorker.register('./service-worker.js?v=318X',{updateViaCache:'none'})
+  navigator.serviceWorker.register('./service-worker.js?v=319X',{updateViaCache:'none'})
     .then(registration=>registration.update())
     .catch(()=>{});
 }
